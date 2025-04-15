8b0f316fae2ba18bdfc34fe18f9abc49a2ec97f5
Kacper <k.g.szmyd@gmail.com>
2025-04-15 15:03:27 +0200

    Fix run.py for static files

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import create_app
 import os
 
-app = create_app()
+app = create_app(static_folder="app/static")
 app.static_folder = os.path.join(os.path.dirname(__file__), "static")
 
 if __name__ == "__main__":
