7287704df071f08fda6ee220993b61b9ac45264d
Kacper <k.g.szmyd@gmail.com>
2025-04-15 14:11:04 +0200

    Add db.create_all() on app start to initialize database on Render

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-from app import create_app
+from app import create_app, db
 import os
 
 app = create_app()
 app.static_folder = os.path.join(os.path.dirname(__file__), "static")
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run()
